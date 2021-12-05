7fd8fdfde963053f5c0a58b1ee0065669442e2d5
Artem Belyi <abelyi467@gmail.com>
2021-12-05 13:25:38 +0300

    create Card

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavbarComponent from '../Navbar/Navbar';
 import Layout from '../Layout/Layout';
 import UserAvatar from '../UserAvatar/UserAvatar';
+import Card from '../Card/Card';
 
 const App = () => {
 
     return (
         <div className="app">
             <NavbarComponent />
+            <Card />
         </div>
     )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import UserAvatar from '../UserAvatar/UserAvatar';
+const styles = require('./Card.module.scss')
 
 const Card = () => {
 
     return (
-        <div className="app">
+        <div className={styles["card-container"]}>
+            <div className={styles["card-container__desc"]}>
+                <div className={styles["card-title"]}>Some article title</div>
+                <div className={styles["card-tags"]}>
+                    <div className={styles["card-tags__item"]}>Tag 1</div>
+                    <div className={styles["card-tags__item"]}>Tag 2</div>
+                </div>
+                <div className={styles["card-text"]}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Nemo, consequuntur ratione nostrum dolorum iusto totam provident aperiam laboriosam enim possimus expedita, 
+                    alias delectus a dolore magnam porro aliquid dolor non.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, error! Labore excepturi amet perspiciatis laboriosam animi delectus, corporis suscipit deserunt libero fugit molestiae distinctio veritatis odio laudantium ad ullam dicta?
+                </div>
+            </div>
+            <div className={styles["card-container__avatar"]}>
+                <UserAvatar />
+            </div>
         </div>
     )
 }
