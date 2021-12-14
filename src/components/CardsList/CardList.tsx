b1dfae4f219115ec5e514211121dea30412cb9a1
Artem Belyi <abelyi467@gmail.com>
2021-12-14 15:28:39 +0300

    create pagination function

--- src/components/CardsList/CardList.tsx
+++ src/components/CardsList/CardList.tsx
@@ -1,33 +1,55 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import PaginationBlog from '../Pagination/Pagination';
-import { Container, Pagination } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { useDispatch } from 'react-redux';
 import { fetchArticlesNoAuth } from '../../store/action-creators/articles';
+import { Article } from '../../types/blog';
+import { setSourceMapRange } from 'typescript';
+
+const splitArray = (arr: Article[]) => {
+    const size = 5
+    let subarray = []
+    for (let i = 0; i <Math.ceil(arr.length/size); i++){
+        subarray[i] = arr.slice((i*size), (i*size) + size);
+    }
+    return subarray
+}
 
 const CardList = () => {
     const {articles, loading, error} = useTypedSelector(state => state.articles)
+    const [page, setPage] = useState(0)
     const dispatch = useDispatch()
+    const showArticle = splitArray([...articles])
 
     useEffect(() => {
         dispatch(fetchArticlesNoAuth())
     }, [dispatch])
 
-    if (loading) return <h1>Идет загрузка</h1>
-    if (error) return <h1>Ошибка загрузки</h1>
+    const showPage = (index: number) => {
+        setPage(index)
+    }
 
-    console.log(articles)
+    const errorMessage = error ? <h1>Идет загрузка</h1> : null
+    const loadingMessage = loading ? <h1>Идет загрузка</h1> : null
+    const articleList = articles.length !== 0 ?  showArticle[page].map(article => {
+        return  <div className="item m-3 d-flex justify-content-center" key={article.slug}><Card article={article}/></div>
+    }) : null
 
     return (
         <Container className='d-flex flex-column justify-content-between' style={{height: '850px'}}>
             <div>
-                {articles.slice(0, 5).map(article => {
-                    return  <div className="item m-3 d-flex justify-content-center" key={article.slug}><Card article={article}/></div>
-                })}
+                {errorMessage}
+                {loadingMessage}
+                {articleList}
             </div>
-            <PaginationBlog />
+            <PaginationBlog
+                page={page}
+                amountPage={showArticle}
+                showPage={showPage}
+            />
         </Container>
     )
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
+import { Article } from '../../types/blog';
 
+interface pageState {
+    page: number
+    amountPage: Article[][]
+    showPage: Function
+}
+const PaginationBlog = (props: pageState) => {
+    const { amountPage, showPage, page } = props
 
-const PaginationBlog = () => {
+    const isActive = (a: number, b: number): boolean => {
+        return a === b ? true : false
+    }
   
     return (
         <Pagination size="sm" className='pt-2 d-flex justify-content-center'>
-            <Pagination.Prev />
-            <Pagination.Item active>{1}</Pagination.Item>
-            <Pagination.Item>{2}</Pagination.Item>
-            <Pagination.Item>{3}</Pagination.Item>
-            <Pagination.Item>{4}</Pagination.Item>
-            <Pagination.Item>{5}</Pagination.Item>
-            <Pagination.Next />
+            <Pagination.Prev 
+                disabled={isActive(page, 0)}
+                onClick={() => showPage(page - 1)}
+            />
+                {amountPage.map((item, index) => 
+                    <Pagination.Item 
+                        key={index} 
+                        onClick={() => showPage(index)}
+                        active={isActive(index, page)}
+                    >
+                        {index + 1}
+                    </Pagination.Item>
+                )}
+            <Pagination.Next 
+                disabled={isActive(page, amountPage.length - 1)}
+                onClick={() => showPage(page + 1)}
+            />
         </Pagination>
     )
 }
