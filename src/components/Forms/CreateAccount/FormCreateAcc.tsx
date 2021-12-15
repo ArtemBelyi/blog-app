e1b234389722c7b2d9225abedf5afd42f2f5de02
Artem Belyi <abelyi467@gmail.com>
2021-12-15 16:19:07 +0300

    create validate forms SignIn, CreateAcc

--- src/components/Forms/CreateAccount/FormCreateAcc.tsx
+++ src/components/Forms/CreateAccount/FormCreateAcc.tsx
@@ -1,85 +1,143 @@
 import React from 'react';
-import { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 const styles = require('../Forms.module.scss');
 
-const FormCreateAcc = () => {
-    const [validated, setValidated] = useState(false);
+interface ValuesForm {
+    userName: string,
+    email: string,
+    password: string,
+    repeatPass: string,
+    checkBox: boolean
+}
 
-    const handleSubmit = (event: any) => {
-      const form = event.currentTarget;
-      console.log(form)
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      setValidated(true);
-    };
+const initialValues: ValuesForm = { 
+    userName: '',
+    email: '',
+    password: '',
+    repeatPass: '',
+    checkBox: false
+};
+
+const FormCreateAcc = () => {
 
     return (
         <Container className='pt-5 d-flex justify-content-center'>
-            <Form className={styles['forms-container']} noValidate validated={validated} onSubmit={handleSubmit}>
+            <Formik
+                initialValues={ initialValues }
+                validationSchema={
+                    Yup.object({
+                        userName: Yup.string()
+                            .min(3, 'Must be 3 characters or more').max(20, 'Must be 20 characters or less').required(),
+                        email: Yup.string()
+                            .email('Invalid email address').required('Required'),
+                        password: Yup.string()
+                            .min(6, 'Must be 6 characters or more')
+                            .max(40, 'Must be 40 characters or less')
+                            .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+                            .required(),
+                        repeatPass: Yup.string()
+                            .oneOf([Yup.ref("password"), null], "Passwords must match").required(),
+                        checkBox: Yup.bool()
+                            .required().oneOf([true], 'You must agree before submitting')
+                    })
+                }
+                onSubmit={(values) => {
+                    console.log(JSON.stringify(values, null, 2));
+                  }}
+            >
+                {({ handleSubmit, handleChange, handleBlur, values, errors, touched, isValid }) => (
+                    <Form className={styles['forms-container']} noValidate onSubmit={handleSubmit}>
 
-            <div className="w-100 mb-4 text-center fs-5">Create new account</div>
+                    <div className="w-100 mb-4 text-center fs-5">Create new account</div>
 
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control 
-                    required
-                    type="text"
-                    placeholder="Username"
-                />
-            </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            name="userName"
+                            placeholder="Username"
+                            value={values.userName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            isValid={touched.userName && !errors.userName}
+                            isInvalid={!!errors.userName}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.userName} 
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    required 
-                    type="email"
-                    placeholder="Enter email" 
-                />
-            </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            name="email"
+                            placeholder="Enter email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            isValid={touched.email && !errors.email}
+                            isInvalid={!!errors.email}
+                        />
+                    </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPass">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    required
-                    type="password"
-                    placeholder="Password" 
-                />
-                <Form.Control.Feedback type="invalid">
-                    You password needs to be at least 6 characters 
-                </Form.Control.Feedback>
-            </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPass">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            isValid={touched.password && !errors.password}
+                            isInvalid={!!errors.password}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.password} 
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-            <Form.Group className="mb-4" controlId="formBasicPassRepeat">
-                <Form.Label>Repeat Password</Form.Label>
-                <Form.Control 
-                    required
-                    type="password" 
-                    placeholder="Password" 
-                />
-                <Form.Control.Feedback type="invalid">
-                    Password must match 
-                </Form.Control.Feedback>
-            </Form.Group>
+                    <Form.Group className="mb-4" controlId="formBasicPassRepeat">
+                        <Form.Label>Repeat Password</Form.Label>
+                        <Form.Control 
+                            type="password"
+                            name="repeatPass"
+                            placeholder="Repeat password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.repeatPass}
+                            isValid={touched.repeatPass && !errors.repeatPass}
+                            isInvalid={!!errors.repeatPass}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.repeatPass} 
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-            <Form.Group className="mb-4" controlId="formBasicCheckbox">
-                <Form.Check 
-                    required
-                    type="checkbox" 
-                    label="I agree to the processing of my personal information"
-                    feedback="You must agree before submitting"
-                    feedbackType="invalid"
-                />
-            </Form.Group>
+                    <Form.Group className="mb-4" controlId="formBasicCheckbox">
+                        <Form.Check 
+                            type="checkbox"
+                            name="checkBox"
+                            label="I agree to the processing of my personal information"
+                            feedback={errors.checkBox}
+                            feedbackType="invalid"
+                            onChange={handleChange}
+                            isInvalid={!!errors.checkBox}
 
-            <Button variant="primary" type="submit" className='w-100 mb-2'>
-                Create
-            </Button>
+                        />
+                    </Form.Group>
 
-            <div className={styles["description"]}>Already have an account? <a href="#">Sign in</a></div>
-            </Form>
+                    <Button variant="primary" type="submit" className='w-100 mb-2'>
+                        Create
+                    </Button>
+
+                    <div className={styles["description"]}>Already have an account? <a href="#">Sign in</a></div>
+                    </Form>
+                )}
+            </Formik>
         </Container>
     )
 }
--- src/components/Forms/SignIn/FormSignIn.tsx
+++ src/components/Forms/SignIn/FormSignIn.tsx
@@ -1,53 +1,85 @@
 import React from 'react';
 import { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 const styles = require('../Forms.module.scss');
 
-const FormSignIn = () => {
-    const [validated, setValidated] = useState(false);
+interface ValuesForm {
+    email: string,
+    password: string,
+}
 
-    const handleSubmit = (event: any) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      setValidated(true);
-    };
+const initialValues: ValuesForm = { 
+    email: '',
+    password: ''
+};
+
+const FormSignIn = () => {
 
     return (
         <Container className='pt-5 d-flex justify-content-center'>
-            <Form className={styles['forms-container']} noValidate validated={validated} onSubmit={handleSubmit}>
+            <Formik
+                initialValues={ initialValues }
+                validationSchema={
+                    Yup.object({
+                        email: Yup.string()
+                            .email('Invalid email address').required('Required'),
+                        password: Yup.string()
+                            .min(6, 'Must be 6 characters or more')
+                            .max(40, 'Must be 40 characters or less')
+                            .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+                            .required()
+                    })
+                }
+                onSubmit={(values) => {
+                    console.log(JSON.stringify(values, null, 2));
+                  }}
+            >
+                {({ handleSubmit, handleChange, handleBlur, values, errors, touched, isValid }) => (
+                    <Form className={styles['forms-container']} noValidate onSubmit={handleSubmit}>
 
-            <div className="w-100 mb-4 text-center fs-5">Sign In</div>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    required 
-                    type="email"
-                    placeholder="Enter email" 
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-4" controlId="formBasicPass">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    required
-                    type="password"
-                    placeholder="Password" 
-                />
-                <Form.Control.Feedback type="invalid">
-                    You password needs to be at least 6 characters 
-                </Form.Control.Feedback>
-            </Form.Group>
-
-            <Button variant="primary" type="submit" className='w-100 mb-2'>
-                Login
-            </Button>
-
-            <div className={styles["description"]}>Don't have an account? <a href="#">Sign in</a></div>
-            </Form>
+                    <div className="w-100 mb-4 text-center fs-5">Sign In</div>
+    
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            name="email"
+                            placeholder="Enter email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            isValid={touched.email && !errors.email}
+                            isInvalid={!!errors.email}
+                        />
+                    </Form.Group>
+    
+                    <Form.Group className="mb-4" controlId="formBasicPass">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            isValid={touched.password && !errors.password}
+                            isInvalid={!!errors.password}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+    
+                    <Button variant="primary" type="submit" className='w-100 mb-2'>
+                        Login
+                    </Button>
+    
+                    <div className={styles["description"]}>Don't have an account? <a href="#">Sign in</a></div>
+                    </Form>
+                )}
+            </Formik>
         </Container>
     )
 }
